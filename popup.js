0c5769aa68949aa968fadb908820f3906b0ab8b6
oov <ilovevipper@gmail.com>
2023-03-07 17:01:22 +0900

    reimplement

--- popup.js
+++ popup.js
@@ -1,23 +1,58 @@
-function paste(e) {
-	window.setTimeout(function(){
-		var v = document.getElementsByTagName('textarea')[0].value.match(/(ttp|http|ttps|https)\:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+/ig);
-		if(v && v.length) {
-			for (var i = 0; i < v.length; ++i) {
-				if (v[i].substring(0,3).toLowerCase() == 'ttp') {
-					v[i] = 'h' + v[i];
-				}
-				chrome.tabs.create({url:v[i]});
-			}
-			window.close();
-		} else {
-			document.body.innerHTML = '<nobr>URL not found from your clipboard data.</nobr>';
-		}
-	}, 0);
+function waitFocus() {
+  return new Promise(resolve => {
+    if (document.hasFocus()) {
+      resolve();
+      return;
+    }
+    window.addEventListener("focus", () => resolve(), { once: true });
+    window.focus();
+  });
 }
 
-window.onload = function () {
-	var textarea = document.getElementsByTagName('textarea')[0];
-	textarea.onpaste = paste;
-	textarea.focus();
-	document.execCommand('paste');
+function readTextFromClipboard() {
+  // It seems that navigator.clipboard.readText does not work correctly with popups.
+  // return navigator.clipboard.readText();
+  return new Promise(resolve => {
+    const textarea = document.createElement('textarea');
+    textarea.style.opacity = '0';
+    textarea.style.position = 'absolute';
+    textarea.style.width = '1px';
+    textarea.style.height = '1px';
+    textarea.style.left = '-1px';
+    textarea.style.top = '0';
+    let resolved = false;
+    const read = () => {
+      if (resolved) {
+        return;
+      }
+      resolved = true;
+      textarea.remove();
+      resolve(textarea.value);
+    };
+    textarea.addEventListener("paste", () => setTimeout(read, 0), { once: true });
+    textarea.addEventListener("input", () => read(), { once: true });
+    textarea.addEventListener("focus", () => document.execCommand('paste'), { once: true });
+    document.body.appendChild(textarea);
+    textarea.focus();
+  });
 }
+
+window.addEventListener("load", async () => {
+  try {
+    await waitFocus();
+    const text = await readTextFromClipboard();
+    const v = text.match(/(https|http|ttps|ttp)\:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+/ig);
+    if (!v || !v.length) {
+      throw new Error('URL not found in your clipboard.');
+    }
+    for (var i = 0; i < v.length; ++i) {
+      if (v[i].substring(0, 3) === 'ttp') {
+        v[i] = 'h' + v[i];
+      }
+      chrome.tabs.create({ url: v[i] });
+    }
+    window.close();
+  } catch (err) {
+    document.querySelector('nobr').innerText = err;
+  }
+}, { once: true });
